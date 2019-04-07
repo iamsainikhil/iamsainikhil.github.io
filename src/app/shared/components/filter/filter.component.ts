971acd62545eba4b4e8d6e78512f514975365c1a
iamsainikhil <sainikhil.bheema@gmail.com>
2019-04-07 08:38:25 -0400

    Update: redesign achievements section

--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -1,24 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FilterService } from './../../services/filter.service';
-import { GAService } from './../../services/ga.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { FilterService } from "./../../services/filter.service";
+import { GAService } from "./../../services/ga.service";
 
 @Component({
-  selector: 'app-filter',
-  templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.css']
+  selector: "app-filter",
+  templateUrl: "./filter.component.html",
+  styleUrls: ["./filter.component.css"]
 })
 export class FilterComponent implements OnInit {
-
   @Input() chipData = [];
   @Input() page: string;
 
   constructor(
     private filterService: FilterService,
     private gaService: GAService
-  ) { }
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   chipSelect(i) {
     for (const j in this.chipData) {
@@ -31,7 +29,10 @@ export class FilterComponent implements OnInit {
     // emit the selected chip name
     this.filterService.chipName.next(this.chipData[i].name);
     // emit ga event
-    this.gaService.emitEvent(`${this.chipData[i].name}-filter-select`, this.page, 'filter-click');
+    this.gaService.emitEvent(
+      `${this.chipData[i].name}-filter-select`,
+      this.page,
+      "filter-click"
+    );
   }
-
 }
