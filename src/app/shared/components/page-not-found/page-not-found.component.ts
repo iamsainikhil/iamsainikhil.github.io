353d61acbc1c29b1a582a2ad771406d459c35335
iamsainikhil <sainikhil.bheema@gmail.com>
2019-04-09 03:04:15 -0400

    Preserve the URL on 404 page

--- src/app/shared/components/page-not-found/page-not-found.component.ts
+++ src/app/shared/components/page-not-found/page-not-found.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router, NavigationEnd, RouterEvent } from "@angular/router";
+import { Location } from "@angular/common";
+import { filter } from "rxjs/operators";
 
 @Component({
-  selector: 'app-page-not-found',
-  templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  selector: "app-page-not-found",
+  templateUrl: "./page-not-found.component.html",
+  styleUrls: ["./page-not-found.component.css"]
 })
-export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class PageNotFoundComponent {
+  constructor(router: Router, location: Location) {
+    router.events
+      .pipe(filter((event: RouterEvent) => event instanceof NavigationEnd))
+      .subscribe(x => {
+        router
+          .navigate(["/404"], { skipLocationChange: true })
+          .then(() => location.go(x.url));
+      });
   }
-
 }
