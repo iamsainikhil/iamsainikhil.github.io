0a7b7d3fb7b29eccfb88318068aa133e19eeb81d
iamsainikhil <sainikhil.bheema@gmail.com>
2021-05-17 11:17:54 -0400

    fix(resume): update pdf viewer on mobile

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -10,6 +10,8 @@ import { GAService } from './../shared/services/ga.service';
 export class ResumeComponent implements OnInit {
   type:string = 'concise'
   conciseSrc: string = "https://firebasestorage.googleapis.com/v0/b/iamsainikhil-portfolio.appspot.com/o/images%2Fresume%2FSaiNikhilBheemanathiniResumeConcise.pdf?alt=media&token=abd4a8e0-69ea-4ba7-9e2d-a17ba0d109d9"
+  conciseDriveURL: string = "https://drive.google.com/file/d/1GumjkNd7wd0T0NBlIJ_S2zexzcPgvIav/preview?usp=sharing"
+  detailedDriveURL: string = "https://drive.google.com/file/d/1jrCfRT60ztxHYqJExXrBIzCundRiEJwT/preview?usp=sharing"
   detailedSrc: string = "https://firebasestorage.googleapis.com/v0/b/iamsainikhil-portfolio.appspot.com/o/images%2Fresume%2FSaiNikhilBheemanathiniResumeDetailed.pdf?alt=media&token=1424aa38-028b-48f8-8ba2-22438e45901b"
 
 
