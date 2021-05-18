33a5c4521f5ea6f3e840208565af173adc4bae47
iamsainikhil <sainikhil.bheema@gmail.com>
2021-05-17 22:06:08 -0400

    chore(resume): update detailed resume drive URL

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -12,7 +12,7 @@ export class ResumeComponent implements OnInit {
   showLoader: boolean = false
   type:string = 'concise'
   conciseDriveURL: string = "https://drive.google.com/file/d/1GumjkNd7wd0T0NBlIJ_S2zexzcPgvIav/preview?usp=sharing"
-  detailedDriveURL: string = "https://drive.google.com/file/d/1jrCfRT60ztxHYqJExXrBIzCundRiEJwT/preview?usp=sharing"
+  detailedDriveURL: string = "https://drive.google.com/file/d/1M7q7EUSms8qzBQJfdVVuS_Cxr9EWfpO4/preview?usp=sharing"
 
 
   constructor(
